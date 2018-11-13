c34fc9a679a444e559e174a98ac9713dee9db68a
solomon1212 <soulgez@gmail.com>
2018-11-12 19:03:15 -0500

    test

--- java.js
+++ java.js
@@ -1,17 +1,18 @@
-// When the user scrolls the page, execute myFunction 
+// When scrolling the page, execute myFunction 
 window.onscroll = function() {myFunction()};
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+// Get the menubar
+var menubar = document.getElementById("menubar");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+// Get the offset position of the menubar
+var sticky = menubar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// Add the sticky class to the menubar when you reach its scroll position. 
+// Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    menubar.classList.add("sticky")
   } else {
-    navbar.classList.remove("sticky");
+    menubar.classList.remove("sticky");
   }
 }
